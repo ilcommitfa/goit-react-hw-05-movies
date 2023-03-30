fadbdebca13eb52236918e1e500e38aa8a767d68
ilcommitfa <110323544+ilcommitfa@users.noreply.github.com>
2023-03-30 21:04:22 +0300

    Delete aaaa.js

--- src/components/aaaa.js
+++
@@ -1,7 +0,0 @@
-useEffect(() => {
-  const fetchMovies = async () => {
-    const trendingMovies = await fetchTrendingMovies(page);
-    setMovies(prevMovies => [...prevMovies, ...trendingMovies]);
-  };
-  fetchMovies();
-}, [page]);
